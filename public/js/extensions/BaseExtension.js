15d821ec585c00ca66d85f70a3229738755806fb
lunaticSKE12 <jamesnapongd@gmail.com>
2022-12-07 10:06:48 +0700

    add viwer and custom model view

---
+++ public/js/extensions/BaseExtension.js
@@ -0,0 +1,62 @@
+export class BaseExtension extends Autodesk.Viewing.Extension {
+    constructor(viewer, options) {
+        super(viewer, options);
+        this._onObjectTreeCreated = (ev) => this.onModelLoaded(ev.model);
+        this._onSelectionChanged = (ev) => this.onSelectionChanged(ev.model, ev.dbIdArray);
+        this._onIsolationChanged = (ev) => this.onIsolationChanged(ev.model, ev.nodeIdArray);
+    }
+
+    load() {
+        this.viewer.addEventListener(Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT, this._onObjectTreeCreated);
+        this.viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, this._onSelectionChanged);
+        this.viewer.addEventListener(Autodesk.Viewing.ISOLATE_EVENT, this._onIsolationChanged);
+        return true;
+    }
+
+    unload() {
+        this.viewer.removeEventListener(Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT, this._onObjectTreeCreated);
+        this.viewer.removeEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, this._onSelectionChanged);
+        this.viewer.removeEventListener(Autodesk.Viewing.ISOLATE_EVENT, this._onIsolationChanged);
+        return true;
+    }
+
+    onModelLoaded(model) {}
+
+    onSelectionChanged(model, dbids) {}
+
+    onIsolationChanged(model, dbids) {}
+
+    findLeafNodes(model) {
+        return new Promise(function (resolve, reject) {
+            model.getObjectTree(function (tree) {
+                let leaves = [];
+                tree.enumNodeChildren(tree.getRootId(), function (dbid) {
+                    if (tree.getChildCount(dbid) === 0) {
+                        // leaves.push(dbid);
+                    }else{
+                        leaves.push(dbid);
+                        // console.log(dbid);
+                    }
+                }, true /* recursively enumerate children's children as well */);
+                resolve(leaves);
+            }, reject);
+        });
+    }
+
+    async findPropertyNames(model) {
+        const dbids = await this.findLeafNodes(model);
+        return new Promise(function (resolve, reject) {
+            model.getBulkProperties(dbids, {}, function (results) {
+                let propNames = new Set();
+                for (const result of results) {
+                    for (const prop of result.properties) {
+                        propNames.add(prop.displayName);
+                    }
+                }
+                resolve(Array.from(propNames.values()));
+            }, reject);
+        });
+    }
+}
+  
+   
